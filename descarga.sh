86fb657ae5e1f301534668caceb45801bd4dda0c
dooros <dooros@posgrado.upv.es>
2018-06-25 13:58:21 +0200

    cambio a python

--- descarga.sh
+++ descarga.sh
@@ -1,7 +1,8 @@
-echo "hola mundo"
-cd /home/user
+#!/bin/bash 
+cd /home/ec2-user
 wget "https://github.com/adolfo24/scarbatch/archive/master.zip"
 unzip master.zip
-cd /home/user/scarbatch-master
+rm master.zip
+cd /home/ec2-user/scarbatch-master
 chmod +x prueba.sh 
 ./prueba.sh
