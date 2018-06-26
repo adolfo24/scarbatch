de7846d3b53ef86cf7a5b2561ee242bb5b73c77c
dooros <dooros@posgrado.upv.es>
2018-06-26 18:41:24 +0200

    scarbatch

--- descarga.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-cd /home/ec2-user
-wget "https://github.com/adolfo24/scarbatch/archive/master.zip"
-unzip master.zip
-rm master.zip
-cd /home/ec2-user/scarbatch-master
-chmod +x prueba.sh 
-./prueba.sh
