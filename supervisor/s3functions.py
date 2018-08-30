ba3bdbe67fec43f6e78f24ca479fd74a494a5c4e
dooros <dooros@posgrado.upv.es>
2018-08-31 01:40:54 +0200

    prueba de s3

--- supervisor/s3functions.py
+++ supervisor/s3functions.py
@@ -14,18 +14,10 @@ class S3():
             error_msg = "Error register job definition."
             print error_msg, error_msg + ": %s" % ce
             raise ce
-    def upload_directory():
-    for root, dirs, files in os.walk(os.environ['SCAR_OUTPUT_DIR']):
-        nested_dir = root.replace(os.environ['SCAR_OUTPUT_DIR'], '')
-        if nested_dir:
-            nested_dir = nested_dir.replace('/','',1) + '/'
-
-        for file in files:
-            complete_file_path = os.path.join(root, file)
-            file = nested_dir + file if nested_dir else file
-            print "[S3_UPLOAD] Going to upload {complete_file_path} to s3 bucket {s3_bucket} as {file}"\
-                .format(complete_file_path=complete_file_path, s3_bucket="pruebascar", file=file)
-            s3_client.upload_file(complete_file_path, "pruebascar", file)
+    def uploadDirectory(path,bucketname):
+        for root,dirs,files in os.walk(path):
+            for file in files:
+                self.client.upload_file(os.path.join(root,file),bucketname,file)
 
 
 if __name__ == "__main__":
@@ -36,7 +28,7 @@ if __name__ == "__main__":
             file1.close()
         os.system('chmod +x '+os.environ['SCAR_INPUT_DIR']+"/script.sh")
     elif(os.environ['MODE']=="FINISH"):
-        s3.upload_directory()
+        s3.uploadDirectory("/tmp",os.environ['BUCKET'])
 
 
 
